9ed30b0eab4affe94279158edfeddc945b00e7c0
subhadiphazra-create <subhadip.hazra@keross.com>
2026-05-07 01:08:29 +0530

    root endpoint added

---
+++ mailservice-server/src/main/java/com/eduai/mailservice/controller/mail/RootController.java
@@ -0,0 +1,17 @@
+package com.eduai.mailservice.controller.mail;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public Map<String, String> root() {
+        return Map.of(
+                "service", "mail-service",
+                "status", "running"
+        );
+    }
+}
